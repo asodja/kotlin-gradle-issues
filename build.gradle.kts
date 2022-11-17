73ab2f84a3cd04ea38e3782e2501114b9e3d0afe
Anže Sodja <asodja@gradle.com>
2022-11-17 12:22:29 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+buildscript {
+    // This throws exception
+    val myProperty: kotlin.String by project
+    println(myProperty)
+}
+
+// This works
+val myProperty: kotlin.String by project
+println(myProperty)
