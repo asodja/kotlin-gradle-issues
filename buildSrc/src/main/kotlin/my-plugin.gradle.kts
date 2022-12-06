41e83ddf25a1a6631dc1c77b77d09c20f2ce6b32
Anže Sodja <asodja@gradle.com>
2022-12-06 17:07:30 +0100

    Simplify test case

--- buildSrc/src/main/kotlin/my-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/my-plugin.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.kotlin.dsl.support.serviceOf
-
 data class Container(
-    val property: Property<String> = gradle.serviceOf<ObjectFactory>().property(String::class.java)
+    val property: Property<String> = objects.property(String::class.java)
 )
